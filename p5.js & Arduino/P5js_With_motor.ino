1fce5486573cb958e961ec97d19ea10db64ac696
Kayley Chery <53196121+kay-134@users.noreply.github.com>
2022-10-16 23:56:02 -0400

    Update P5js_With_motor.ino

--- p5.js & Arduino/P5js_With_motor.ino
+++ p5.js & Arduino/P5js_With_motor.ino
@@ -2,7 +2,6 @@
 
 Servo motor;
 int ang = 0;
-int brightness = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -13,8 +12,7 @@ void setup() {
 void loop() {
   int pot1 = analogRead(A0);
   int pot2 = analogRead(A1);
-  int light = analogRead(A2);
-
+  
   if (Serial.available() > 0) {
     int p5info = Serial.read();
     
@@ -22,8 +20,5 @@ void loop() {
     Serial.print(",");
     Serial.print(pot2);
     Serial.print(",");
-    Serial.println(light);
-    brightness = p5info;
     }
-    analogWrite(10, brightness);
 }
